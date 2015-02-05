abca8199ae9bf05c18f0e6409b4ad8e39345c2a7
MartinFuc <ma_fuchs@hotmail.com>
2015-02-06 00:51:41 +0100

    Added floodfill function to combine a bunch of circles to one

--- Fuchs-gdx-core/src/com/mygdx/game/Ex2.java
+++ Fuchs-gdx-core/src/com/mygdx/game/Ex2.java
@@ -43,6 +43,94 @@ public class Ex2 implements ApplicationListener
 		return output;
 	}
 	
+	private Mat floodfill(Mat matrix)
+	{
+		int[][] filled = new int[matrix.rows()][matrix.cols()];
+		Mat output2 = Mat.zeros(matrix.size(), matrix.type());
+		int[] temp = {0,0};
+		int[] temp2 = {0,0};
+		ArrayList<int[]> stack = new ArrayList<int[]>();
+		double accx = 0;
+		double accy = 0;
+		double count = 0;
+		double[] ar = new double[] {0};
+        final double threshold = 0.005*5;
+        int area = 0;
+        
+		for( int j = 0; j < matrix.rows(); j++ )
+        {
+            for( int i = 0; i < matrix.cols(); i++ )
+            {
+            	ar = matrix.get(j,i);
+                area = filled[j][i];
+                filled[j][i] = 1;
+                if(ar[0] > threshold && area == 0)
+                {
+                	temp[0] = j;
+                	temp[1] = i;
+                	stack.add(temp.clone());
+                	while(stack.isEmpty() == false)
+                	{
+                		temp = stack.remove(0);
+                		ar = matrix.get(temp[0],temp[1]);
+                		accx += temp[0]*ar[0];
+                		accy += temp[1]*ar[0];
+                		count += ar[0];
+                		
+                		if(temp[0] < matrix.rows()-1 && temp[1] < matrix.cols()-1 && temp[0] > 0 && temp[1] > 0)
+                		{
+	                		temp2[0] = temp[0]+1;
+	                		temp2[1] = temp[1];
+	                		
+	                		ar = matrix.get(temp2[0],temp2[1]);
+	                		area = filled[temp2[0]][temp2[1]];
+	                		if(ar[0] > threshold && area == 0)
+	                		{
+	                			filled[temp2[0]][temp2[1]] = 1;
+	                			stack.add(temp2.clone());
+	                		}
+	                		
+	                		temp2[0] = temp[0]-1;
+	                		ar = matrix.get(temp2[0],temp2[1]);
+	                		area = filled[temp2[0]][temp2[1]];
+	                		if(ar[0] > threshold && area == 0)
+	                		{
+	                			filled[temp2[0]][temp2[1]] = 1;
+	                			stack.add(temp2.clone());
+	                		}
+	                		
+	                		temp2[0] = temp[0];
+	                		temp2[1] = temp[1]+1;
+	                		ar = matrix.get(temp2[0],temp2[1]);
+	                		area = filled[temp2[0]][temp2[1]];
+	                		if(ar[0] > threshold && area == 0)
+	                		{
+	                			filled[temp2[0]][temp2[1]] = 1;
+	                			stack.add(temp2.clone());
+	                		}
+	                		
+	                		temp2[1] = temp[1]-1;
+	                		ar = matrix.get(temp2[0],temp2[1]);
+	                		area = filled[temp2[0]][temp2[1]];
+	                		if(ar[0] > threshold && area == 0)
+	                		{
+	                			filled[temp2[0]][temp2[1]] = 1;
+	                			stack.add(temp2.clone());
+	                		}
+                		}
+                	}
+                	//output2.put((int) (accy/count+0.5),(int) (accx/count+0.5),1);
+                	if(count > 0.5)
+                		Core.circle(image, new Point((int) (accy/count+0.5), (int) (accx/count+0.5)), 5, new Scalar(255, 255, 255));
+                	accx = 0;
+                	accy = 0;
+                	count = 0;
+                }
+            }
+        }
+		return output2;
+	}
+	
 	@Override
 	public void render() 
     {
@@ -59,8 +147,11 @@ public class Ex2 implements ApplicationListener
             del.release();
         }
         output = alfa;
-    
+        
+        floodfill(output);
+        
         /// Drawing a circle around corners
+    /*
         for( int j = 0; j < output.rows(); j++ )
         {
             for( int i = 0; i < output.cols(); i++ )
@@ -73,6 +164,7 @@ public class Ex2 implements ApplicationListener
                 }
             }
         }
+    */
 		UtilAR.imDrawBackground(image);
 	}
 	
