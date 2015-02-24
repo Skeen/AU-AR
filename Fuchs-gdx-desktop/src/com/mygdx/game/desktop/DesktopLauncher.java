4140b2a97de9e312431cf561a9a0a5edcfb161b2
MartinFuc <ma_fuchs@hotmail.com>
2015-02-24 13:02:42 +0100

    new UtilAR

--- Fuchs-gdx-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Fuchs-gdx-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,12 +7,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 //import com.mygdx.game.Ex2;
 import com.mygdx.game.Ex2Cheat;
 import com.mygdx.game.Ex2;
+import com.mygdx.game.Ex3;
 //import com.mygdx.game.HelloCV;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		System.loadLibrary("opencv_java2410");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Ex2Cheat(), config);
+		new LwjglApplication(new Ex3(), config);
 	}
 }
